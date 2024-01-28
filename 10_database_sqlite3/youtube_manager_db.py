b15e373aa1e011c1ba82eea9b7ba9424cc875a50
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2024-01-28 13:50:35 +0530

    Add SQLite database functionality to YouTube manager app

---
+++ 10_database_sqlite3/youtube_manager_db.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+conn = sqlite3.connect('youtube_videos.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS videos (
+               id INTEGER PRIMARY KEY,
+               name TEXT NOT NULL,
+               time TEXT NOT NULL
+    )
+''')
+
+def list_videos():
+    cursor.execute("SELECT * FROM videos")
+    for row in cursor.fetchall():
+        print(row)
+
+def add_video(name, time):
+    cursor.execute("INSERT INTO videos (name, time) VALUES (?, ?)", (name, time))
+    conn.commit()
+
+def update_video(video_id, new_name, new_time):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (new_name, new_time, video_id))
+    conn.commit()
+
+def delete_video(video_id):
+    cursor.execute("DELETE FROM videos where id = ?", (video_id,))
+    conn.commit()
+
+def main():
+    while True:
+        print("\n Youtube manager app with DB")
+        print("1. List Videos")
+        print("2. Add Videos")
+        print("3. Update Videos")
+        print("4. Delete Videos")
+        print("5. exit app")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            list_videos()
+        elif choice == '2':
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            add_video(name, time)
+        elif choice == '3':
+            video_id = input("Enter video ID to update: ")
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            update_video(video_id, name, time)
+        elif choice == '4':
+            video_id = input("Enter video ID to delete: ")
+            delete_video(video_id)
+        elif choice == '5':
+            break
+        else:
+            print("Invalid Choice ")
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
