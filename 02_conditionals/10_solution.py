6ec39ee8b421f785ad6add3ca24e69e4087dfb3d
Gautham Vanjre <vanjregautham@gmail.com>
2024-03-25 12:43:28 +0530

    solution: answer for question number 10 in 02_conditionals folder

---
+++ 02_conditionals/10_solution.py
@@ -0,0 +1,11 @@
+species = "Dog"
+age = 10
+
+if species == "Dog" and age < 2:
+    print("Puppy Food")
+elif species == "Dog" and age > 2:
+    print("Senior Dog food")
+elif species == "Cat" and age < 5:
+    print("Kitten food")
+elif species == "Cat" and age > 5: 
+    print("Senior Cat food")
